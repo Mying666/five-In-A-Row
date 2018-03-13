5b1f30651f9d3d31dfc68b0ce06204fe5da436c6
liang <466035017@qq.com>
2018-03-13 16:39:20 +0800

    基本功能实现，明天再改

--- js/game.js
+++ js/game.js
@@ -8,6 +8,7 @@ class fiveInRow {
         this.myWin = {}
         this.comWin = {}
         this.count = 0            // 统计赢法计数
+        this.points = []          // 二维数组，标记每一个点的状态，0为空、1为人、2为电脑
 
         this.grid = 15            // 标准五子棋为15*15格
         this.box = document.getElementById('game')
@@ -19,6 +20,7 @@ class fiveInRow {
         this.statistics()
         this.createCheckerboard()
         this.init()
+        this.events()
         this.resize()
     }
     // 赢法统计
@@ -103,12 +105,11 @@ class fiveInRow {
         bgImg.onload = function () {
             me.ctx.drawImage(bgImg, 0, 0, me.maxWidth, me.maxWidth);
             me.createGrid()
-
-            me.createChess(7, 7, false)
-            me.createChess(7, 6, true)
-            me.createChess(6, 7, true)
-            me.createChess(8, 7, true)
-            me.createChess(7, 8, true)
+            // me.createChess(7, 7, false)
+            // me.createChess(7, 6, true)
+            // me.createChess(6, 7, true)
+            // me.createChess(8, 7, true)
+            // me.createChess(7, 8, true)
         }
     }
     createGrid () {
@@ -142,9 +143,12 @@ class fiveInRow {
         me.ctx.fill();
     }
 
-    // 创建棋子
+    /** 创建棋子
+     * @param {number} i 
+     * @param {number} j 
+     * @param {boolen} myTurn 
+     */
     createChess (i, j, myTurn) {
-        console.log(i, j, myTurn)
         let me = this
         let interval = me.interval
         let margin = interval / 2
@@ -163,6 +167,125 @@ class fiveInRow {
         }
         me.ctx.fillStyle = color
         me.ctx.fill()
+    }
+
+    events () {
+        let me = this
+        this.box.addEventListener('click', e => {
+            if (me.over || !me.myTurn) {
+                return
+            }
+            let x = e.offsetX,
+                y = e.offsetY,
+                i = Math.floor(x / this.interval),
+                j = Math.floor(y / this.interval)
+            if (me.points[i][j] === 0) {
+                me.createChess(i, j, true)
+                me.points[i][j] = 1
+
+                // 遍历点击的点的赢法序号,人此序号赢法加一,电脑此赢法超出
+                for (let n in me.wins[i][j]){
+                    me.myWin[n]++
+                    me.comWin[n] = 6
+                    if (me.myWin[n] === 5) {
+                        alert('大神你赢了')
+                        me.over = true
+                        return
+                    }
+                }
+                if (!me.over) {
+                    me.myTurn = false
+                    me.comAi()
+                }
+            }
+        })
+    }
+
+    /** 程序落子
+     * @description 需要判断人和电脑分别将要完成那种赢法
+     *              如果人要完成某种赢法则优先防守（在该赢法其他点上落子）
+     *              如果电脑比人完成的多，则优先进攻（继续完成某种赢法）
+     */
+    comAi () {
+        let me = this
+        let myScore = [],
+            comScore = [],
+            max = 0,
+            ci = 7,
+            cj = 7
+        for (let i = 0; i < 15; i++) {
+            myScore[i] = []
+            comScore[i] = []
+            for (let j = 0; j < 15; j++) {
+                myScore[i][j] = 0
+                comScore[i][j] = 0
+            }
+        }
+        // 遍历所有的点，找出最应该下子的点
+        this.points.forEach((p, indexI) => {
+            p.forEach((j,indexJ) => {
+                if (j === 0) {
+                    for (let n in me.wins[indexI][indexJ]) {
+                        if (me.myWin[n] === 1) {
+                            myScore[indexI][indexJ] += 10
+                        } else if (me.myWin[n] === 2) {
+                            myScore[indexI][indexJ] += 100
+                        } else if (me.myWin[n] === 3) {
+                            myScore[indexI][indexJ] += 1000
+                        } else if (me.myWin[n] === 4) {
+                            myScore[indexI][indexJ] = 10000
+                        }
+
+                        if (me.comWin[n] === 1) {
+                            comScore[indexI][indexJ] += 11
+                        } else if (me.comWin[n] === 2) {
+                            comScore[indexI][indexJ] += 101
+                        } else if (me.comWin[n] === 3) {
+                            comScore[indexI][indexJ] += 1001
+                        } else if (me.comWin[n] === 4) {
+                            comScore[indexI][indexJ] += 10001
+                        }
+                    }
+                    if (myScore[indexI][indexJ] > max) {
+                        max = myScore[indexI][indexJ]
+                        ci = indexI
+                        cj = indexJ
+                    } else if (myScore[indexI][indexJ] === max) {
+                        if (comScore[indexI][indexJ] > comScore[ci][cj]) {
+                            ci = indexI
+                            cj = indexJ
+                        }
+                    }
+
+                    if (comScore[indexI][indexJ] > max) {
+                        max = comScore[indexI][indexJ]
+                        ci = indexI
+                        cj = indexJ
+                    } else if (comScore[indexI][indexJ] === max) {
+                        if (myScore[indexI][indexJ] > myScore[ci][cj]) {
+                            ci = indexI
+                            cj = indexJ
+                        }
+                    }
+                }
+            })
+        })
+        // 落子并标记
+        me.createChess(ci, cj, false)
+        me.points[ci][cj] = 2
+        // 检查是否胜利
+        for (let n in me.wins[ci][cj]){
+            me.comWin[n]++
+            me.myWin[n] = 6
+            if (me.comWin[n] === 5) {
+                alert('技术不到家，还得再练练！')
+                me.over = true
+                return
+            }
+        }
+        if (!me.over) {
+            me.myTurn = true
+        }
     }
 
     resize () {
